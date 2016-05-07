bb4787f3fd7d7dd01004572c64e344502e04c3e4
Josh Chorlton <jchorlton@gmail.com>
2016-05-07 13:10:11 -0700

    added log-in checking logic

--- routes.go
+++ routes.go
@@ -1,11 +1,17 @@
 package financejc
 
 import (
+	"errors"
+	"fmt"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/emicklei/go-restful"
 	"github.com/emicklei/go-restful/swagger"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 
 	"auth"
+	"credentials"
 	"handlers"
 )
 
@@ -15,6 +21,33 @@ func getGaeURL() string {
 	} else {
 		return "https://financejc.appspot.com"
 	}
+}
+
+func loggedInFilter(request *restful.Request, response *restful.Response, chain *restful.FilterChain) {
+	c := appengine.NewContext(request.Request)
+	log.Debugf(c, "starting auth")
+	cookie, err := request.Request.Cookie("auth")
+	if err == nil {
+		log.Debugf(c, "Unparsed: %s", cookie.Value)
+		token, err := jwt.Parse(cookie.Value, func(token *jwt.Token) (interface{}, error) {
+			// Don't forget to validate the alg is what you expect:
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, errors.New(fmt.Sprintf("Unexpected signing method: %v", token.Header["alg"]))
+			}
+			return []byte(credentials.JWT_SIGNING_KEY), nil
+		})
+		if err == nil && token.Valid {
+			request.SetAttribute("userId", token.Claims["userId"])
+			log.Debugf(c, "parsed token, user id: %+v", request.Attribute("userId"))
+		} else if err != nil {
+			log.Errorf(c, "error parsing jwt: %+v", err)
+			return
+		}
+	} else {
+		response.WriteErrorString(401, "401: Not Authorized")
+		return
+	}
+	chain.ProcessFilter(request, response)
 }
 
 func init() {
@@ -33,6 +66,9 @@ func init() {
 		Doc("Get all currencies").
 		Operation("GetCurrencies").
 		Writes(struct{ ISO4217 string }{"Name"}))
+	ws.Route(ws.GET("/accountGroup").Filter(loggedInFilter).To(handlers.GetAccountGroups).
+		Doc("Get all account groups").
+		Operation("GetAccountGroups"))
 	restful.Add(ws)
 
 	config := swagger.Config{
