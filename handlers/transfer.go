eda6a0b8695b603af77a801c374ca76abea825ef
Josh Chorlton <jchorlton@gmail.com>
2016-07-13 23:16:19 -0700

    import from import folder

--- handlers/transfer.go
+++ handlers/transfer.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"io/ioutil"
 	"net/http"
 	"os"
+	"path"
 
 	"github.com/emicklei/go-restful"
 	"google.golang.org/appengine"
@@ -11,22 +13,32 @@ import (
 	"github.com/jchorl/financejc/transfer"
 )
 
+const autoImportPath = "../import"
+
 func Transfer(request *restful.Request, response *restful.Response) {
 	userId := request.Attribute("userId").(string)
 	c := appengine.NewContext(request.Request)
 
-	file, err := os.Open("/home/josh/downloads/US.qif")
+	files, err := ioutil.ReadDir(autoImportPath)
 	if err != nil {
-		log.Errorf(c, "error opening file: %+v", err)
+		log.Errorf(c, "error listing files: %+v", err)
 		response.WriteError(http.StatusInternalServerError, err)
 		return
 	}
-	defer file.Close()
+	for _, f := range files {
+		file, err := os.Open(path.Join(autoImportPath, f.Name()))
+		if err != nil {
+			log.Errorf(c, "error opening file: %+v", err)
+			response.WriteError(http.StatusInternalServerError, err)
+			return
+		}
+		defer file.Close()
 
-	err = transfer.TransferQIF(c, userId, file)
-	if err != nil {
-		log.Errorf(c, "error importing file: %+v", err)
-		response.WriteError(http.StatusInternalServerError, err)
-		return
+		err = transfer.TransferQIF(c, userId, file)
+		if err != nil {
+			log.Errorf(c, "error importing file: %+v", err)
+			response.WriteError(http.StatusInternalServerError, err)
+			return
+		}
 	}
 }
