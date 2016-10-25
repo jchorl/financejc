b0d1a6324d0b6d08d03d6125a78c9e8d9ea7553b
Josh Chorlton <jchorlton@gmail.com>
2016-10-25 23:29:54 +0800

    not really sure what is in this change

--- client/src/components/accountList/index.js
+++ client/src/components/accountList/index.js
@@ -6,44 +6,44 @@ import { toCurrency } from '../../utils';
 import styles from './accountList.css';
 
 @connect((state) => {
-	return {
-		accounts: state.account
-	}
+  return {
+    accounts: state.account
+  }
 })
 export default class AccountList extends React.Component {
-	static propTypes = {
-		accounts: React.PropTypes.object.isRequired,
-		onSelect: React.PropTypes.func,
-		selected: React.PropTypes.number
-	}
+  static propTypes = {
+    accounts: React.PropTypes.object.isRequired,
+    onSelect: React.PropTypes.func,
+    selected: React.PropTypes.number
+  }
 
-	render () {
-		const {
-			accounts,
-			onSelect,
-			selected
-		} = this.props;
+  render () {
+    const {
+      accounts,
+      onSelect,
+      selected
+    } = this.props;
 
-		return (
-			<div>
-				<h3 className={ styles.accountsTitle }>Accounts</h3>
-				<div>
-					{ accounts.map(account => {
-						let selectedClass = {};
-						selectedClass[styles.selected] = selected === account.get('id');
-						return (
-							<button key={ account.get('id') } className={ classNames(styles.accountButton, selectedClass) } onClick={ onSelect.bind(this, account.get('id')) }>
-								<div className={ styles.accountName }>
-									{ account.get('name') }
-								</div>
-								<div className={ styles.accountBalance }>
-									Balance: { toCurrency(account.get('futureValue'), account.get('currency')) }
-								</div>
-							</button>
-						)
-					}).valueSeq().toArray() }
-				</div>
-			</div>
-		)
-	}
+    return (
+      <div>
+        <h3 className={ styles.accountsTitle }>Accounts</h3>
+        <div>
+          { accounts.map(account => {
+            let selectedClass = {};
+            selectedClass[styles.selected] = selected === account.get('id');
+            return (
+              <button key={ account.get('id') } className={ classNames(styles.accountButton, selectedClass) } onClick={ onSelect.bind(this, account.get('id')) }>
+                <div className={ styles.accountName }>
+                  { account.get('name') }
+                </div>
+                <div className={ styles.accountInfo }>
+                  Balance: { toCurrency(account.get('futureValue'), account.get('currency')) }
+                </div>
+              </button>
+            )
+          }).valueSeq().toArray() }
+        </div>
+      </div>
+    )
+  }
 }
