ca6b7642d442c6b8de6b9e1265c7b745663ef8cd
Josh Chorlton <jchorlton@gmail.com>
2016-10-25 23:19:12 +0800

    added nav bar

--- client/src/components/googleLoginButton/index.js
+++ client/src/components/googleLoginButton/index.js
@@ -1,50 +1,52 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
 import { login } from '../../actions'
+import styles from './googleLoginButton.css';
 
 function attachToButton(dispatcher) {
-	return function() {
-		gapi.load('auth2', function(){
-			let auth2 = gapi.auth2.init({
-				client_id: '900762983843-0ih1hv6b4mf4ql847ini51hhfc4svqoc.apps.googleusercontent.com',
-				scope: 'email'
-			});
-			let btnEl = document.getElementById("googleBtn");
-			if (btnEl) {
-				auth2.attachClickHandler(btnEl, {},
-					function(googleUser) {
-						dispatcher(login(googleUser));
-					}, function(error) {
-						console.log(error);
-					});
-			}
-		});
-	}
+  return function() {
+    gapi.load('auth2', function(){
+      let auth2 = gapi.auth2.init({
+        client_id: '900762983843-0ih1hv6b4mf4ql847ini51hhfc4svqoc.apps.googleusercontent.com',
+        scope: 'email'
+      });
+      let btnEl = document.getElementById("googleBtn");
+      if (btnEl) {
+        auth2.attachClickHandler(btnEl, {},
+          function(googleUser) {
+            dispatcher(login(googleUser));
+          }, function(error) {
+            console.log(error);
+          });
+      }
+    });
+  }
 }
 
 @connect()
 export default class GoogleLoginButton extends React.Component {
-	static propTypes = {
-		dispatch: React.PropTypes.func.isRequired
-	}
+  static propTypes = {
+    dispatch: React.PropTypes.func.isRequired
+  }
 
-	componentDidMount() {
-		if (document.readyState === 'complete') {
-			attachToButton(this.props.dispatch);
-		} else {
-			window.onload = attachToButton(this.props.dispatch);
-		}
-	}
+  componentDidMount() {
+    if (document.readyState === 'complete') {
+      attachToButton(this.props.dispatch);
+    } else {
+      window.onload = attachToButton(this.props.dispatch);
+    }
+  }
 
-	componentWillUnmount() {
-		window.onload = undefined;
-	}
+  componentWillUnmount() {
+    window.onload = undefined;
+  }
 
-	render() {
-		return (
-			<div id="googleBtn">
-				<span>Login with Google</span>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div id="googleBtn" className={ styles.loginButton } >
+        <span>Login</span>
+      </div>
+    );
+  }
 }
--- client/src/components/index/index.js
+++ client/src/components/index/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { connect } from 'react-redux';
+
 import { fetchAuth } from '../../actions';
 import AccountsPage from '../accountsPage';
 import GoogleLoginButton from '../googleLoginButton';
@@ -8,23 +9,33 @@ import Loader from '../loader';
 import styles from './styles.css';
 
 @connect((state) => {
-	return { auth: state.auth }
+  return { auth: state.auth }
 })
 export default class App extends React.Component {
-	static propTypes = {
-		auth: React.PropTypes.object.isRequired
-	}
+  static propTypes = {
+    auth: React.PropTypes.object.isRequired
+  }
 
-	constructor(props) {
-		super(props);
-		props.dispatch(fetchAuth());
-	}
+  constructor(props) {
+    super(props);
+    props.dispatch(fetchAuth());
+  }
 
-	render () {
-		return (
-			<Loader loading={ !this.props.auth.fetched }>
-				{ this.props.auth.authd ? <AccountsPage /> : <GoogleLoginButton /> }
-			</Loader>
-		)
-	}
+  render () {
+    return (
+      <div>
+        <nav className={ styles.navBar }>
+          <div>
+            FinanceJC
+          </div>
+          <div>
+            { this.props.auth.authd ? <span>Welcome</span> : <GoogleLoginButton /> }
+          </div>
+        </nav>
+        <Loader loading={ !this.props.auth.fetched }>
+          { this.props.auth.authd ? <AccountsPage /> : <h1>Welcome to FinanceJC</h1> }
+        </Loader>
+      </div>
+    )
+  }
 }
