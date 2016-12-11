d4e04cabc235039c503dda0a48a7cc8df5212fb2
Josh Chorlton <jchorlton@gmail.com>
2016-12-12 00:38:37 +0800

    favicon

--- client/webpack.production.config.js
+++ client/webpack.production.config.js
@@ -8,55 +8,56 @@ var APP_DIR = path.resolve(__dirname, 'src');
 var constants = require('./src/components/index/constants');
 
 var config = {
-  devtool: 'source-map',
-  entry: APP_DIR,
-  output: {
-    path: BUILD_DIR,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel?cacheDirectory=true,presets[]=es2015,presets[]=react,presets[]=stage-1,plugins[]=transform-decorators-legacy',
-      },
-      {
-        test : /\.css$/,
-        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]!postcss-loader'
-      }
+    devtool: 'source-map',
+    entry: APP_DIR,
+    output: {
+        path: BUILD_DIR,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test : /\.jsx?/,
+                include : APP_DIR,
+                loader : 'babel?cacheDirectory=true,presets[]=es2015,presets[]=react,presets[]=stage-1,plugins[]=transform-decorators-legacy',
+            },
+            {
+                test : /\.css$/,
+                loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]!postcss-loader'
+            }
+        ]
+    },
+    postcss: [
+        require('postcss-constants')({
+            defaults: constants
+        }),
+        require('autoprefixer'),
+        require('precss'),
+        require('postcss-nested'),
+        require('lost')
+    ],
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                screw_ie8: true
+            }
+        }),
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new HtmlWebpackPlugin({
+            template: APP_DIR + '/index.html',
+            favicon: APP_DIR + '/favicon.png',
+            filename: 'index.html',
+            minify: {},
+            inject: 'body'
+        })
     ]
-  },
-  postcss: [
-    require('postcss-constants')({
-      defaults: constants
-    }),
-    require('autoprefixer'),
-    require('precss'),
-    require('postcss-nested'),
-    require('lost')
-  ],
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        screw_ie8: true
-      }
-    }),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new HtmlWebpackPlugin({
-      template: APP_DIR + '/index.html',
-      filename: 'index.html',
-      minify: {},
-      inject: 'body'
-    })
-  ]
 };
 
 module.exports = config;
