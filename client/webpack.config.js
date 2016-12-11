2d8d5457a1c9175e16200c9e39a1de764dc26694
Josh Chorlton <jchorlton@gmail.com>
2016-12-11 21:15:52 +0800

    added favicon

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,45 +8,46 @@ var APP_DIR = path.resolve(__dirname, 'src');
 var constants = require('./src/components/index/constants');
 
 var config = {
-  entry: APP_DIR,
-  output: {
-    path: BUILD_DIR,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loaders : ['babel?cacheDirectory=true,presets[]=es2015,presets[]=react,presets[]=stage-1,plugins[]=transform-decorators-legacy', 'eslint-loader'],
-      },
-      {
-        test : /\.css$/,
-        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]!postcss-loader'
-      }
+    entry: APP_DIR,
+    output: {
+        path: BUILD_DIR,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test : /\.jsx?/,
+                include : APP_DIR,
+                loaders : ['babel?cacheDirectory=true,presets[]=es2015,presets[]=react,presets[]=stage-1,plugins[]=transform-decorators-legacy', 'eslint-loader'],
+            },
+            {
+                test : /\.css$/,
+                loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]!postcss-loader'
+            }
+        ]
+    },
+    eslint: {
+        configFile: './.eslintrc',
+        fix: true
+    },
+    postcss: [
+        require('postcss-constants')({
+            defaults: constants
+        }),
+        require('autoprefixer'),
+        require('precss'),
+        require('postcss-nested'),
+        require('lost')
+    ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: APP_DIR + '/index.html',
+            filename: 'index.html',
+            favicon: APP_DIR + '/favicon.png',
+            inject: 'body'
+        })
     ]
-  },
-  eslint: {
-    configFile: './.eslintrc',
-    fix: true
-  },
-  postcss: [
-    require('postcss-constants')({
-      defaults: constants
-    }),
-    require('autoprefixer'),
-    require('precss'),
-    require('postcss-nested'),
-    require('lost')
-  ],
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: APP_DIR + '/index.html',
-      filename: 'index.html',
-      inject: 'body'
-    })
-  ]
 };
 
 module.exports = config;
