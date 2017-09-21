9b6c4a1e2dc3f94c9152a14f68fddd403df91f25
Josh Chorlton <jchorlton@gmail.com>
2017-09-21 03:12:51 -0400

    Fixing cert script

--- scripts/renew_certs.sh
+++ scripts/renew_certs.sh
@@ -1,6 +1,5 @@
 docker run -it --rm --name certbot \
-	-v "/etc/letsencrypt:/etc/letsencrypt" \
-	-v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+	-v financejcletsencrypt:/etc/letsencrypt \
 	-v wellknown:/wellknown \
 	quay.io/letsencrypt/letsencrypt:latest \
 	certonly -a webroot --webroot-path=/wellknown --noninteractive --agree-tos --keep --expand -d finance.joshchorlton.com --email=josh@joshchorlton.com
